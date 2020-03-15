36cff7dde35abd6e54c42773aea6357c8dc9b7f5
Abdul Latif Mubasir <bj.angel119@gmail.com>
2020-03-15 13:40:39 +0700

    menyempurnakan style dan membuat animasi agar dinamis

--- js/script.js
+++ js/script.js
@@ -1,14 +1,54 @@
 
+// variabel
+const icon = document.querySelector('.icon');
+const sidebar = document.getElementById('sidebar');
 
+icon.addEventListener('click' , () => {
+    if(!icon.classList.contains('open')) {
+        icon.classList.add('open');
+        icon.style.transform = 'rotate(180deg)';
+        sidebar.style.animation = 'sidebarAppear .8s forwards';
+    } else {
+        icon.classList.remove('open');
+        icon.style.transform = 'rotate(-180deg)';
+        sidebar.style.animation = 'sidebarHidden .8s forwards';
+    }
+})
 
+// parralax
 
-// parralawax
-let lebarJendela = window.innerWidth;
+window.addEventListener('scroll' , () => {
+    let lebarJendela = window.innerWidth;
+    let wScroll = window.pageYOffset;
 
-if(lebarJendela > 400) {
+    const tentangKontent    = document.querySelector('.tentang p');
+    const tentangImg        = document.querySelector('.tentang img');
+    const jurusan           = document.querySelectorAll('.jurusan .card-style');
+    const fasilitas         = document.querySelectorAll('.fasilitas .card-style');
+    const footerImg         = document.querySelector('.footer .row img');
 
-    let wScroll = document.pageYOffset;
+    if(lebarJendela > 400) {
     
-} else {
+        
+    } else {
+        console.log(wScroll);
+        
+        if(wScroll > 480) {
+            tentangKontent.style.animation = 'landingLeft .7s forwards';
+            tentangImg.style.animation = 'landingRight .7s forwards';
+        }
 
-}
+        if(wScroll > 950) {
+            for(i = 0 ; i < jurusan.length ; i++) {
+                jurusan[i].style.animation = 'landingTop .7s .' + i * 4 + 's forwards';
+            }
+        }
+
+        if(wScroll > 2300) {
+            for(i = 0 ; i < fasilitas.length ; i++) {
+                fasilitas[i].style.animation = 'landingTop .7s .' + i * 4 + 's forwards';
+            }
+        }
+    }
+    
+})
